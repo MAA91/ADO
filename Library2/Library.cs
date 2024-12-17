793f3a2eb954c07e44e7d107dc1230482da86638
MAA91 <Sanya13082007@gmail.com>
2024-12-17 22:56:49 +0300

    Select in Library2 DONE

---
+++ Library2/Library.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Library2
+{
+    internal class Library
+    {
+        public static string connectionString = "";
+        static SqlConnection connection = null;
+        static readonly string delimetr1 = "\n------------------------";
+        static readonly string delimetr2 = "\n========================";
+
+        static  Library()
+        { 
+            connectionString = ConfigurationManager.ConnectionStrings["Library"].ConnectionString;
+            connection = new SqlConnection(connectionString);
+            Console.WriteLine(connectionString);
+        }
+
+        public static void Select(string filders, string tables, string condition="", int padding = 20)
+        {
+            string cmd = $"SELECT {filders} FROM {tables}";
+            if (condition.Length > 0) cmd += $" WHERE {condition}";
+            cmd += ";";
+            SqlCommand command = new SqlCommand(cmd, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                Console.WriteLine(delimetr2);
+                for(int i = 0; i < reader.FieldCount; i++) 
+                    Console.Write(reader.GetName(i).PadRight(padding));
+                Console.WriteLine(delimetr1);
+                while (reader.Read())
+                {
+                    for(int i = 0; i < reader.FieldCount; i++)
+                        Console.Write(reader[i].ToString().PadRight(padding));
+                    Console.WriteLine();
+                }
+                Console.WriteLine(delimetr2);
+            }
+            reader.Close();
+            connection.Close();
+        }
+    }
+}
---
+++ Querys/SQLQuery1- BooksCOUNT for Author.sql
@@ -0,0 +1,7 @@
+﻿USE Library;
+GO
+
+SELECT COUNT(book_id) AS 'Books count', first_name+' '+last_name AS 'Author'
+FROM	Books, Authors
+WHERE	author=author_id
+GROUP BY last_name, first_name
